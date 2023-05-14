e6ced860a309d75463afd97c1296e2db03ae64df
Ninusj <128705752+Ninusj@users.noreply.github.com>
2023-05-14 22:34:30 +0200

    hej

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <ClerkProvider  {...pageProps}>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </ClerkProvider>
   );
 };
 
